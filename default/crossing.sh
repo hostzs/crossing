04113e3db313543a9ea2c9d2baf7cfb68e31b556
hostzs <46411561+hostzs@users.noreply.github.com>
2026-09-15 10:28:25 +0800

    Update crossing.sh

--- default/crossing.sh
+++ default/crossing.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "开始下载真实数据"
+echo "开始下载"
 
 curl -L -o source.txt https://raw.githubusercontent.com/iflyelf/gwf/main/direct.txt
 
 echo "下载完成"
 
-echo "文件大小："
+echo "总行数："
 wc -l source.txt
+
+echo "前 20 行："
+head -20 source.txt
